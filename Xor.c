88a7cdde49abc4009c4d8759aaaad50df1d05b62
0xFreDi <62937543+oveeauki@users.noreply.github.com>
2025-04-05 08:26:15 +0300

    Update Xor.c

--- Xor.c
+++ Xor.c
@@ -2,18 +2,18 @@
 #include "stdio.h"
 #include "string.h"
 
-__u_char xor(char data[],char key){
+char* xor(char* data,u_char key){
   int len = strlen(data);
   for(size_t i=0;i<len;i++){
-   data[i] = (data[i] ^= key);
-}
+    data[i] ^= key;
+  }
   return data;
 }
 
 signed main(int argc,char* argv[]){
-  char data[] = "hello";
-  printf("%s\n",data);
-  char key = 0xA;
-  char res = xor(data,key);
-  printf("%s\n",res);
+  u_char key = 0xf;
+  char* input = strdup(argv[1]);
+  char *res = xor(input,key);
+  puts(res);
+  return(0);
 }
