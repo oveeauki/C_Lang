12b02aa2359a055488b88615a5ad9e9f1f518c81
0xFreDi <62937543+oveeauki@users.noreply.github.com>
2025-11-27 12:02:02 +0200

    Update morepointers.c

--- morepointers.c
+++ morepointers.c
@@ -7,7 +7,7 @@
 
 void** lparalloc(void*** arr,size_t m){
   *arr = malloc(sizeof(void*)*m);
-  memset(*arr,0,sizeof(void*)*m);
+  memset(*arr,0x0,sizeof(*arr));
   for(short i=0;i<m;i++){
     (*arr)[i] = malloc(1000);
   }
@@ -16,8 +16,11 @@ void** lparalloc(void*** arr,size_t m){
 
 signed main(int argc,char** argv){ 
   void** jaa;
-  lparalloc(&jaa,1);  
-  printf("Enter\n\u279c "); scanf("%s %d",(char*)jaa[0],(int*)&jaa[1]);
-  printf("%s %d len(%d)\n",(char*)jaa[0],(int*)jaa[1],((size_t)strlen((char*)jaa[0])));
+  lparalloc(&jaa,1);
+  printf("Enter String and Int\n\u279c "); 
+  scanf("%s %d",(char*)jaa[0],(int*)&jaa[1]);
+  printf("Str (%s) Int(%d) strlen(%d)\n",(char*)jaa[0],(int*)jaa[1],((size_t)strlen((char*)jaa[0])));
+  memset(jaa,0x0,sizeof(jaa));
+  free(jaa);
   return(0);
 }
